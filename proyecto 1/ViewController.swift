cdabe030c1ff606b261211a6c452e56d9191b751
Laboratorio UNAM-Apple9 <laboratoriounam-apple09@macbook-pro-de-laboratorio-9.local>
2018-03-06 09:11:07 -0600

    Initial Commit

---
+++ proyecto 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proyecto 1
+//
+//  Created by Laboratorio UNAM-Apple09 on 06/03/18.
+//  Copyright © 2018 Adrieli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
